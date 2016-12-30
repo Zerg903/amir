bbee0a82891d359cdf22a4f910ac1e40062cc97e
Sergey Fetisov <zerg903@mail.ru>
2016-12-30 15:34:34 +0300

    Загрузка картинок

---
+++ assets/js/common.js
@@ -0,0 +1,18 @@
+﻿
+(function () {
+
+  var options = {
+    history: false,
+    focus: false,
+    bgOpacity: 0.9,
+    showAnimationDuration: 0,
+    hideAnimationDuration: 0
+  };
+
+  window.openPhotoSwipe = function (items) {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+  };
+
+}());
